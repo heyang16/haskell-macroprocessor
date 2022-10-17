3505b5b0270c884414e9922710445dca18202a94
Heyang <heyang16@users.noreply.github.com>
2022-10-17 18:07:15 +0100

    Did until expand, more comments and test cases needed

--- MP.hs
+++ MP.hs
@@ -14,35 +14,52 @@ separators
 
 -----------------------------------------------------
 
+-- Given a string k and list of pairs with a string as the 1st element, 
+-- returns the list of the 2nd elements whose 1st element matches k
 lookUp :: String -> [(String, a)] -> [a]
-lookUp k [] = []
-lookUp k ((x, y) : xys) 
-  = if k == x 
-      then 
-        y: lookUp k xys 
-      else 
-        lookUp k xys
+lookUp k xys = [y | (x, y) <- xys, k == x]
 
 splitText :: [Char] -> String -> (String, [String])
-splitText
-  = undefined
+splitText ks "" = ("", [""])
+splitText ks (c : cs)
+  | c `elem` ks = (c : a, "" : b)
+  | otherwise = (a, (c : b1) : bs)
+  where
+    (a, b) = splitText ks cs
+    (b1: bs) = b
 
 combine :: String -> [String] -> [String]
-combine
-  = undefined
+combine "" s = s
+combine k [""] = [k]
+combine (k:ks) (s:ss)
+  = s : [k] : combine ks ss
+     
 
 getKeywordDefs :: [String] -> KeywordDefs
-getKeywordDefs
-  = undefined
+getKeywordDefs [] = []
+getKeywordDefs (s:ss)
+  = (c1, a) : getKeywordDefs ss
+  where
+    (k, cs) = splitText separators s
+    (c1 : c2) = cs
+    a = concat (combine (tail k) c2)
 
 expand :: FileContents -> FileContents -> FileContents
-expand
-  = undefined
+expand ori keystring
+  = concat (combine sep rep_words)
+  where 
+    (sep, ori_words) = splitText separators ori
+    keywords = getKeywordDefs(snd(splitText "\n" keystring))
+    rep_words = [replaceWord ori_word keywords | ori_word <- ori_words]
 
 -- You may wish to uncomment and implement this helper function
 -- when implementing expand
--- replaceWord :: String -> KeywordDefs -> String
-
+replaceWord :: String -> KeywordDefs -> String
+replaceWord "" keydefs = ""
+replaceWord ori keydefs 
+  = if null replaced then ori else head replaced
+  where
+    replaced = [v | (d, v) <- keydefs, ori == d]
 -----------------------------------------------------
 
 -- The provided main program which uses your functions to merge a
